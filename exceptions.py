e8a1064139298e3fb3f001655433d625373921e6
dilipvaleti <57723484+dilipvaleti@users.noreply.github.com>
2020-03-14 21:55:16 +0530

    add all files

---
+++ exceptions.py
@@ -0,0 +1,8 @@
+"""Exception handlers."""
+class ProductNotLoadedException(Exception):
+    """Exception class to raise when products are not loaded already."""
+
+    def __init__(self, expression=None, message="Products are not loaded yet."):
+        """initialize."""
+        self.expression = expression
+        self.message = message
